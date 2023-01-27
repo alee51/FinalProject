3b84b93d6e1af80e374e02c2e463a42ce270ab5b
alee51 <122579716+alee51@users.noreply.github.com>
2023-01-26 21:51:33 -0500

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -1,32 +1,4 @@
 import java.util.*;
-//object class for tasks
-class Task {
-  private String name;
-  private int deadline;
-  
-  //constructor
-  public Task (String name, int deadline) {
-    this.name = name;
-    this.deadline = deadline;
-  }
-  
-  public String getName() {
-    return this.name;
-  }
-  
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public int getDeadline() {
-    return this.deadline;
-  }
-  
-  public String toString() {
-    return String.format("%-30s %d", getName(), getDeadline());
-  }
-}
-
 public class FinalProject {
   //main method
   public static void main(String[] args) {
